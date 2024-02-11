9d172cc1690c6110f5aa06a5b3feb6008d04752e
danil-danil-golovin <147338322+danil-danil-golovin@users.noreply.github.com>
2024-02-11 20:32:10 +0300

    Revert "ООП2"

--- src/Bicycle.java
+++
@@ -1,8 +0,0 @@
-public class Bicycle extends WheeledTranspotr{
-    public Bicycle(String modelName){
-        super(modelName, 2);
-    }
-    public Bicycle(String modelName, int wheelsCount){
-        super(modelName, wheelsCount);
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-     Car car = new Car("car1");
-     Car car1 = new Car("car2");
 
-     Track track = new Track("track1");
-     Track track1 = new Track("track2");
-
-     Bicycle bicycle = new Bicycle("bicycle1");
-     Bicycle bicycle1 = new Bicycle("bicycle2");
-
-     Transport[] transports = {
-             car,
-             car1,
-             track,
-             track1,
-             bicycle,
-             bicycle1
-     };
-     ServiceStation station = new ServiceStation();
-     for (Transport transport: transports){
-         station.check(transport);
-     }
     }
 }
